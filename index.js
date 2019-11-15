381cc725faeab811ade5b22e0b808c22abe2fad8
Lars-The-Gamer <57726036+Lars-The-Gamer@users.noreply.github.com>
2019-11-15 22:22:06 +0100

    init

--- index.js
+++ index.js
@@ -41,7 +41,11 @@ bot.on("message", async message => {
 
     if(command === `${prefix}ok`){
 
-        return message.channel.send("Iedereen die zich gisteren (11/11/2019) heeft geregistreerd voor de HaZe clan is nu lid! Er zijn een paar aanpassingen op jullie Discord! Veel plezier!");
+        var botEmbed = new discord.RichEmbed()
+        .setDescription("Iedereen die zich gisteren (11/11/2019) heeft geregistreerd voor de HaZe clan is nu lid! \n Er zijn een paar aanpassingen op jullie Discord! Veel plezier!")
+        .setColor("#79ff00");
+
+        return message.channel.send(botEmbed);
 
     }
 
@@ -57,7 +61,11 @@ bot.on("message", async message => {
 
     if(command === `${prefix}leden`){
 
-        return message.author.send("**HaZe LEDEN** \n\n **Leiders:** \n 1. Lars Dobbelaere \n 2. HaZe Looped \n 3. HaZe Casper \n\n **Leden:** \n 1. HaZe Lias \n 2. HaZe Lucas \n 3. HaZe SAM MEY YT \n 4. HaZe Woutty \n\n **Totaal:** \n 7 leden");
+        var botEmbed = new discord.RichEmbed()
+            .setDescription("**HaZe LEDEN** \n\n **Leiders:** \n 1. Lars Dobbelaere \n 2. HaZe Looped \n 3. HaZe Casper \n\n **Leden:** \n 1. HaZe Lias \n 2. HaZe Lucas \n 3. HaZe SAM MEY YT \n 4. HaZe Woutty \n\n **Totaal:** \n 7 leden")
+            .setColor("#79ff00");
+
+        return message.author.send(botEmbed);
 
     }
 
